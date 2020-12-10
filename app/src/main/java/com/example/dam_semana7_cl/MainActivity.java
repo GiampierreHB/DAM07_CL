111892de9af1e44a7af4035d2f7579d9e4dd58d1
GiampierreHB <68082714+GiampierreHB@users.noreply.github.com>
2020-12-10 17:10:56 -0500

    Constraint Layout semana 7

--- app/src/main/java/com/example/dam_semana7_cl/MainActivity.java
+++ app/src/main/java/com/example/dam_semana7_cl/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.dam_semana7_cl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin = findViewById(R.id.button_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NotasActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
